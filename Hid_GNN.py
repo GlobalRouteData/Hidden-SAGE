cf92b04fd5c389a909780a1869af9200be2dc74b
GlobalRouteData <115964986+GlobalRouteData@users.noreply.github.com>
2024-12-24 22:20:46 +0800

    Update Hid_GNN.py

--- Hid_GNN.py
+++ Hid_GNN.py
@@ -14,36 +14,36 @@ from sklearn.metrics import f1_score, recall_score
 # 2014 dataset
 # num_nodes = 46939
 # num_nodes = 46960
-# num_features = 8  # 节点特征维度  2014年没有AS霸权
+# num_features = 8  # 
 #
-# # message_links = '../train_data/triple_link_samples/triple_changed_message_links.txt'
-# message_links = '../train_data/common_link_samples/common_changed_message_links.txt'
+# # message_links = ''
+# message_links = ''
 #
-# # sample_links = '../train_data/triple_link_samples/triple_changed_sample_links.txt'
-# sample_links = '../train_data/common_link_samples/common_changed_sample_links.txt'
+# # sample_links = ''
+# sample_links = ''
 #
-# # node_features = '../train_data/triple_link_samples/triple_node_features.csv'
-# node_features = '../train_data/common_link_samples/common_node_features.csv'
+# # node_features = ''
+# node_features = ''
 #
-# # triple_count_file = 'triple_statics.txt'
-# common_count_file = 'common_statics.txt'
+# # triple_count_file = ''
+# common_count_file = ''
 
 # 2023 dataset
 # num_nodes = 75829
 num_nodes = 76187
-num_features = 9  # 节点特征维度  2023年有AS霸权
+num_features = 9  # 
 
-# message_links = r'D:\IDEs\pycharmProjects\HiddenLinkInfer\2023\triple_changed_message_links.txt'
-message_links = r'D:\IDEs\pycharmProjects\HiddenLinkInfer\2023\common_changed_message_links.txt'
+# message_links = r''
+message_links = r''
 
-# sample_links = r'D:\IDEs\pycharmProjects\HiddenLinkInfer\2023\triple_changed_sample_links.txt'
-sample_links = r'D:\IDEs\pycharmProjects\HiddenLinkInfer\2023\common_changed_sample_links.txt'
+# sample_links = r''
+sample_links = r''
 
-# node_features = r'D:\IDEs\pycharmProjects\HiddenLinkInfer\2023\triple_node_features_1.csv'
-node_features = r'D:\IDEs\pycharmProjects\HiddenLinkInfer\2023\common_node_features.csv'
+# node_features = r''
+node_features = r''
 
-# triple_count_file = '2023_triple_statics.txt'
-common_count_file = '2023_common_statics.txt'
+# triple_count_file = ''
+common_count_file = ''
 
 src_nodes = []
 dst_nodes = []
@@ -67,7 +67,7 @@ with open(sample_links, 'r+') as f:
         labeled_links.append(labeled_link)
         labels.append(int(label))
 
-# 打乱样本，并随机取80%作为训练集，20%作为测试集
+
 dataframe1 = pd.DataFrame()
 dataframe1['labeled_links'] = labeled_links
 dataframe2 = pd.DataFrame()
@@ -80,7 +80,6 @@ train_src_node = []
 train_dst_node = []
 
 
-# 训练集
 for i in train['labeled_links']:
     AS1 = i.split('|')[0]
     AS2 = i.split('|')[1]
@@ -91,7 +90,7 @@ train_src_node = torch.tensor(train_src_node)
 train_dst_node = torch.tensor(train_dst_node)
 train_labels = torch.tensor(train_labels)
 
-# 测试集
+
 test_src_node = []
 test_dst_node = []
 for i in test['labeled_links']:
@@ -106,23 +105,21 @@ test_dst_node = torch.tensor(test_dst_node)
 test_labels = torch.tensor(test_labels)
 
 
-# 创建图
+
 g = dgl.DGLGraph()
 g.add_nodes(num_nodes)
 g.add_edges(src_nodes, dst_nodes)
 
-# 读取节点特征的 CSV 文件
+
 with open(node_features, 'r') as csvfile:
     reader = csv.reader(csvfile)
 
-    # 读取节点特征数据
     node_features_data = [list(map(float, row)) for row in reader]
 
-# 将节点特征添加到图 g
+
 g.ndata['feat'] = torch.tensor(node_features_data)
 
 
-# 创建GraphSAGE模型
 class GraphSAGE(nn.Module):
     def __init__(self, in_feats, hidden_size, out_feats):
         super(GraphSAGE, self).__init__()
@@ -144,7 +141,7 @@ class EdgeClassifier(nn.Module):
         self.sig = nn.Sigmoid()
 
     def forward(self, src_nodes, dst_nodes, node_embed):
-        # 获取节点和边的索引，然后通过索引获取相应的节点特征和边特征
+
         src_node_embed = node_embed[src_nodes]
         dst_node_embed = node_embed[dst_nodes]
 
@@ -155,17 +152,17 @@ class EdgeClassifier(nn.Module):
         return logits
 
 
-# 初始化图神经网络和边分类器
+
 graph_sage_model = GraphSAGE(num_features, 32, 16)
 edge_classifier_model = EdgeClassifier(16, 1)
-# 定义损失函数和优化器
+
 criterion = nn.BCEWithLogitsLoss()
 optimizer = optim.Adam(edge_classifier_model.parameters(), lr=0.01, betas=(0.5, 0.9), weight_decay=1e-6)
 
 node_embedding = graph_sage_model(g, g.ndata['feat'])
 node_embedding = node_embedding.detach()
 print("nodeebd:", node_embedding)
-# 训练模型
+
 num_epochs = 200
 for epoch in range(num_epochs):
 
@@ -184,24 +181,24 @@ torch.save(edge_classifier_model.state_dict(), 'edge_classifier_model_paras.pth'
 
 with open(common_count_file,'a+') as w:
     with torch.no_grad():
-        # 获取测试集上的预测结果
+
         test_logits = edge_classifier_model(test_src_node, test_dst_node, node_embedding)
         test_logits = torch.sigmoid(test_logits)
         test_predicted_labels = torch.round(test_logits)
         test_predicted_labels = torch.squeeze(test_predicted_labels)
-        # 计算测试集准确率
+
         test_accuracy = (test_predicted_labels == test_labels).float().mean().item()
         print(f'Test Accuracy: {test_accuracy:.4f}')
 
-        # 将预测标签和真实标签转换为numpy数组
+
         test_predicted_labels_numpy = test_predicted_labels.cpu().numpy()
         test_labels_numpy = test_labels.cpu().numpy()
 
-        # 计算F1 score和recall
+
         f1 = f1_score(test_labels_numpy, test_predicted_labels_numpy)
         recall = recall_score(test_labels_numpy, test_predicted_labels_numpy)
 
-        # 打印结果
+
         print(f'F1 Score: {f1:.4f}')
         print(f'Recall: {recall:.4f}')
 
